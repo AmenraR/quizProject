27016421c215a6910d5ae1a9af42838ca80bbf68
Ezra Alarid <eja.117@gmail.com>
2014-12-19 12:16:02 -0800

    my failure of a project, here it is

--- app/src/main/java/android/alaride/quizproject/Questions.java
+++ app/src/main/java/android/alaride/quizproject/Questions.java
@@ -1,10 +1,37 @@
 package android.alaride.quizproject;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
 public class Questions {
 
+    public static Answer answer;
+    public count = new count;
 
-    public Questions(){
+    public Questions() {
 
+        public answer = String("True");
+    }
+    public static Answer get(){
+        if(answer == null){
+            answer = new Answer();
+            Toast toast = Toast.makeText(context, correct, 100);
+            toast.show();
+            count++;
+        }
+        return answer;
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences mPrefs = getSharedPreferences();
+        mCurViewMode = mPrefs.getInt("view_mode", DAY_VIEW_MODE);
+    }
+
+
+    public String getAnswer(){
+        return answer[1];
+    }
 }
-
-}
+//my overrides aren't working so I'm some what screwed.
